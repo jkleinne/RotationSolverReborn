af15c37d4dd7387e14e12add00a3b85e7c2ac4b4
jkleinne <jkq@jkleinne.com>
2026-06-02 14:53:22 -0400

    feat(duties): add UMAD ultimate territory indicator

--- RotationSolver.Basic/Rotations/CustomRotation_OtherInfo.cs
+++ RotationSolver.Basic/Rotations/CustomRotation_OtherInfo.cs
@@ -1512,6 +1512,12 @@ public partial class CustomRotation
 	public static bool IsInFRU => DataCenter.IsInFRU;
 
 	///<summary>
+	/// Is player in UMAD duty.
+	///</summary>
+	[Description("Is in UMAD duty")]
+	public static bool IsInUMAD => DataCenter.IsInUMAD;
+
+	///<summary>
 	/// Is player in COD duty.
 	///</summary>
 	[Description("Is in FRU duty")]
